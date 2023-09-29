1bda00d47ec5baceb7eca717ad1fd17035710fd7
Grace-Cordova <gcordova@smith.edu>
2023-09-29 15:24:54 -0400

    Declutter!

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,68 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+public class Conversation {
+    public static void main(String[] args) {
+        // You will start the conversation here.
+        int num_rounds = -1;
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
-  }
+
+        String[] transcript = new String[2 * num_rounds + 2];
+
+        String[] p_response = {
+            "mm-hmm",
+            "ok!",
+            "yup!",
+        };
+        String[] i_response = {
+            "I like that!",
+            "I'm not sure",
+            "I don't do that"
+        };
+        String[] u_response = {
+            "You think so?",
+            "You're funny!",
+            "Anyways..."
+        };
+        String[] q_response = {
+            "Good question! I'll have to think about that",
+            "Hmmm... I'm not too sure",
+            "Huh?"
+        };
+
+        Scanner input = new Scanner(System.in);
+        Random r = new Random();
+        System.out.println("How many rounds of conversation would you like to have?");
+        num_rounds = input.nextInt();
+        input.nextLine();
+        System.out.println("Hi, there! What's on your mind today?");
+
+        for (int round = 0; round < num_rounds; round++) {
+            String response = input.nextLine().toLowerCase();
+            String[] split_list = response.split(" ");
+            int i = r.nextInt(2);
+            for (String element: split_list) {
+                if (element.equals("you")) {
+                    System.out.println(i_response[i]);
+                } else if (element.equals(" i ")) {
+                    System.out.println(u_response[i]);
+                } else if (element.equals("who")) {
+                    System.out.println(q_response[i]);
+                } else if (element.equals("what")) {
+                    System.out.println(q_response[i]);
+                } else if (element.equals("where")) {
+                    System.out.println(q_response[i]);
+                } else if (element.equals("when")) {
+                    System.out.println(q_response[i]);
+                } else if (element.equals("why")) {
+                    System.out.println(q_response[i]);
+                } else if (element.equals("how")) {
+                    System.out.println(q_response[i]);
+                } else {
+                    System.out.println(p_response[i]);
+                }
+
+            }
+        }
+    }
 }
--- reflection.md
+++ reflection.md
@@ -5,3 +5,8 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+During this assignment I talked with Dani about our code. During the course of the week I went to office hours on Tuesday morning and I went to tutoring on Monday? These are my approximations. 
+Initially, I think that Java is very tempermental and particular and I'm already starting to hate brackets but that is ok. 
+I think I'm still at a point where I think through things very procedurally. When we talked in class on Thursday about memory models I feel like I slowly began to see how everything is understood through objects. 
+
