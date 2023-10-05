4ad639010ae0cf5d3a877a3f41771aedd0aa296d
Grace-Cordova <gcordova@smith.edu>
2023-10-05 14:40:33 -0400

    Submit1

--- Conversation.java
+++ Conversation.java
@@ -6,9 +6,6 @@ public class Conversation {
         // You will start the conversation here.
         int num_rounds = -1;
 
-
-        String[] transcript = new String[2 * num_rounds + 2];
-
         String[] p_response = {
             "mm-hmm",
             "ok!",
@@ -32,37 +29,52 @@ public class Conversation {
 
         Scanner input = new Scanner(System.in);
         Random r = new Random();
+        String[] transcript = new String[2 * num_rounds + 2];
         System.out.println("How many rounds of conversation would you like to have?");
         num_rounds = input.nextInt();
         input.nextLine();
         System.out.println("Hi, there! What's on your mind today?");
+        transcript[0] = ("Hi, there! What's on your mind today!");
 
         for (int round = 0; round < num_rounds; round++) {
             String response = input.nextLine().toLowerCase();
+            transcript[2*round + 1] = response;
             String[] split_list = response.split(" ");
             int i = r.nextInt(2);
             for (String element: split_list) {
                 if (element.equals("you")) {
                     System.out.println(i_response[i]);
+                    transcript[2*round + 2] = i_response[i];
                 } else if (element.equals(" i ")) {
                     System.out.println(u_response[i]);
+                    transcript[2*round + 2] = u_response[i];
                 } else if (element.equals("who")) {
                     System.out.println(q_response[i]);
+                    transcript[2*round + 2] = q_response[i];
                 } else if (element.equals("what")) {
                     System.out.println(q_response[i]);
+                    transcript[2*round + 2] = q_response[i];
                 } else if (element.equals("where")) {
                     System.out.println(q_response[i]);
+                    transcript[2*round + 2] = q_response[i];
                 } else if (element.equals("when")) {
                     System.out.println(q_response[i]);
+                    transcript[2*round + 2] = q_response[i];
                 } else if (element.equals("why")) {
                     System.out.println(q_response[i]);
+                    transcript[2*round + 2] = q_response[i];
                 } else if (element.equals("how")) {
                     System.out.println(q_response[i]);
+                    transcript[2*round + 2] = q_response[i];
                 } else {
                     System.out.println(p_response[i]);
+                    transcript[2*round + 2] = p_response[i];
                 }
 
             }
+        System.out.println("Thanks for chatting with me! See ya later!");
+        transcript[-1] = ("Thanks for chatting with me! See ya later!");
+        System.out.println(transcript);
         }
     }
 }
--- reflection.md
+++ reflection.md
@@ -6,7 +6,9 @@ Use this file to record your reflection on this assignment.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
 
-During this assignment I talked with Dani about our code. During the course of the week I went to office hours on Tuesday morning and I went to tutoring on Monday? These are my approximations. 
+During this assignment I talked with Dani about our code. 
+During the course of the week I went to office hours on Tuesday morning and I went to tutoring on Monday? These are my approximations. I also met with Jordan on Friday. We discussed that I took a different approach when building the chat bot and I have decided to keep that in. 
 Initially, I think that Java is very tempermental and particular and I'm already starting to hate brackets but that is ok. 
 I think I'm still at a point where I think through things very procedurally. When we talked in class on Thursday about memory models I feel like I slowly began to see how everything is understood through objects. 
+I was able to get everything to work however my code has a lot of redundancies. Through further rounds of iteration I think I could simplify my code by creating a for loop that runs through each of my stop words with a nested for loop that runs through each word in the user's repsonse. 
 
